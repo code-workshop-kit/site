5c0319061614afccb5bcfb41d0720909034ba940
jorenbroekema <joren.broekema@gmail.com>
2020-11-01 17:07:58 +0100

    styling fixes, faq section

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ if (videoElems.length > 0) {
 window.addEventListener("load", () => {
   import("./loadDankMonoFont.js");
 });
+
+const setupCopyBtn = () => {
+  let copyTimeout;
+  const copyBtn = document.getElementById("copy-npm-btn");
+  const copyBtnInner = copyBtn.innerHTML;
+  copyBtn.addEventListener("click", () => {
+    clearTimeout(copyTimeout);
+    navigator.clipboard.writeText(copyBtn.innerText);
+    copyBtn.innerText = "Copied!";
+    copyTimeout = setTimeout(() => (copyBtn.innerHTML = copyBtnInner), 2000);
+  });
+};
+setupCopyBtn();
